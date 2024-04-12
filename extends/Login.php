ef3d0049022531c7d2a9aeb4e395dbc25628fea0
Eliel Godoy <eliel.a.m.godoy@gmail.com>
2024-04-11 22:36:50 -0300

    Utilizando autoload

---
+++ extends/Login.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Extends;
+
+require __DIR__.'/../vendor/autoload.php';
+
+use Extends\UsuarioFactory;
+
+class Login
+{
+    public function executar($lo, $se)
+    {
+        $login = $lo;
+        $senha = $se;
+
+        $perfil = 'professor';
+        $usuario = UsuarioFactory::criar($perfil);
+
+        if ($usuario->autenticar($login, $senha)) {
+            $autorizacoes = $usuario->autorizar();
+            echo "Bem-vindo, $login! Você tem acesso às seguintes funcionalidades: " . implode(', ', $autorizacoes);
+        } else {
+            echo 'Login ou senha incorretos.';
+        }
+    }
+}
+
+$login = new Login();
+$login->executar('eu', '123');
